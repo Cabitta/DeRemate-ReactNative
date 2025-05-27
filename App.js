8aad6e6215ecc0d2773590f14bcb26cea4edce8d
Rodrigo Moens <rodrigo_moens@hotmail.com>
2025-05-26 23:10:32 -0300

    Pantalla Principal

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
+import React from 'react';
+import image from './assets/Logo.png'
+//import { Button } from 'react-native-web';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hagamos una gran app!</Text>
+      <Image
+        source={image}
+        style={[styles.image, { resizeMode: 'contain' }]}
+      />
+      <Text style={styles.title}>El sabor que queres, en el momento que lo necesitas</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button}
+      onPress={()=> console.log("Parte en progreso")}>
+      <Text style={styles.buttonText}>Iniciar Sesion</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}
+      onPress={()=> console.log("Parte en progreso")}>
+      <Text style={styles.buttonText}>Registrarse</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
+//'#ffffe0'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffdab9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontFamily: 'sans-serif',
+    fontSize: 20,
+    color: '#000000'
+  },
+  image: {
+    height: 200,
+    width: 200,
+    alignSelf: 'center'
+  },
+  button:{
+    backgroundColor: "#8b0000",
+    padding: 10,
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30
+  },
+  buttonText:{
+    fontSize: 15,
+    color: "#ffffff",
+    width: 120,
+    height: 25,
+    textAlign: "center"
+  }
 });
