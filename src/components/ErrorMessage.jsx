2df03dadc7a9e98ba5cf9e7c1008e86f2e8a32e0
Franco Cabanillas <fcabanillas@uade.edu.ar>
2025-06-15 17:10:54 -0300

    Componente de pantalla de mensaje de error con boton

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,22 @@
+import { Text } from 'react-native-paper'
+import { View } from 'react-native'
+import PropTypes from 'prop-types'
+import { COLORS} from '../theme/appTheme'
+import ButtonPaper from './ButtonPaper'
+
+const ErrorMessage = ({message = "Ocurrio un error. Intentelo de nuevo más tarde.", onPress}) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text variant="titleLarge" style={{ color: COLORS.primaryButton, textAlign: 'center'}}>
+            {message}
+        </Text>
+    <ButtonPaper title={"Reintentar"} onPress={onPress}/>       
+    </View>
+  )
+}
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string,
+}
+
+export default ErrorMessage
