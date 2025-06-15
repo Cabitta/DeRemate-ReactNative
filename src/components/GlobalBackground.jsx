02468f89c530846f03b230972dbcbfa45631a898
Franco Cabanillas <fcabanillas@uade.edu.ar>
2025-06-15 17:12:01 -0300

    creacion de componente global para establecer un fondo de pantalla predeterminado

---
+++ src/components/GlobalBackground.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import { View } from 'react-native'
+import { COLORS } from '../theme/appTheme'
+
+const GlobalBackground = ({ children, color = COLORS.primaryBackground }) => {
+  return (
+    <View style={{ flex: 1, backgroundColor: color }}>
+      {children}
+    </View>
+  )
+}
+
+GlobalBackground.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string,
+}
+
+export default GlobalBackground
