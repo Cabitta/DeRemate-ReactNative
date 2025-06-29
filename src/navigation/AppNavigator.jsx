c8ab3bb5980081b0cbe9bcf034fa6785fa2734cb
Franco Cabanillas <fr.cabanillas7@gmail.com>
2025-06-29 16:08:58 -0300

    RENOMBRAR: Cambia el nombre de la pantalla "DeliveryHistoryScreen" a "DeliveryHistoryStack" en la navegación

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -57,7 +57,7 @@ function AppTabs() {
         }}
         />
       <Tab.Screen
-        name="DeliveryHistoryScreen"
+        name="DeliveryHistoryStack"
         component={DeliveryHistoryStack}
         options={({ navigation }) => ({
           title: "Historial de Entregas",
