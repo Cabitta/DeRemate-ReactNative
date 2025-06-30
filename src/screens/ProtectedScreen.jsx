5fa22a3a6d4bbd3e8c6d8798b61dbe52e80339b8
Franco Cabanillas <fr.cabanillas7@gmail.com>
2025-06-30 19:22:16 -0300

    Agregar funcionalidad para abrir Google Maps en AvailableRoutesCard y ProtectedScreen; refactorizar DialogPaperComponent para usar texto dinámico en botones

--- src/screens/ProtectedScreen.jsx
+++ src/screens/ProtectedScreen.jsx
@@ -1,9 +1,10 @@
-import { View, StyleSheet, Platform, Linking } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigation } from "@react-navigation/native";
 import ButtonPaper from "../components/ButtonPaper";
 import { Text } from "react-native-paper";
+import { openGoogleMaps } from "../utils/helpers";
 
 const ProtectedScreen = () => {
   const { user, logout } = useContext(AuthContext);
@@ -17,30 +18,6 @@ const ProtectedScreen = () => {
     }
   };
 
-  const openGoogleMaps = () => {
-    const location = "UADE";
-    const browserUrl = `https://www.google.com/maps/search/?api=1&query=${location}`;
-    const url = Platform.select({
-      ios: `maps:0,0?q=${location}`,
-      android: `geo:0,0?q=${location}`,
-    });
-
-    if (Platform.OS === "web") {
-      window.open(browserUrl, "_blank");
-      return;
-    }
-
-    Linking.canOpenURL(url)
-      .then((supported) => {
-        if (supported) {
-          return Linking.openURL(url);
-        } else {
-          return Linking.openURL(browserUrl);
-        }
-      })
-      .catch((err) => console.error("Error al abrir Google Maps:", err));
-  };
-
   return (
     <View style={styles.container}>
       {user && (
@@ -50,7 +27,7 @@ const ProtectedScreen = () => {
           </Text>
         </View>
       )}
-      <ButtonPaper title={"Mi ruta"} onPress={openGoogleMaps} />
+      <ButtonPaper title={"Mi ruta"} onPress={() => openGoogleMaps("UADE")} />
       <ButtonPaper title={"Cerrar Sesión"} onPress={handleLogout} />
     </View>
   );
