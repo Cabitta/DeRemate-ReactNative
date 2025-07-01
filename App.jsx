5c8f5691390e86a7c6d78ebbd79ff7072a3c0849
Lucas <lucaspistolesi07@gmail.com>
2025-07-01 17:00:24 -0300

    removed expo-notifications warn

--- App.jsx
+++ App.jsx
@@ -2,6 +2,13 @@ import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext'; 
 import { PaperProvider } from 'react-native-paper';
 
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+  'expo-notifications: Android Push notifications',
+  '`expo-notifications` functionality is not fully supported in Expo Go'
+]);
+
 export default function App() {
   return (
     <AuthProvider>
